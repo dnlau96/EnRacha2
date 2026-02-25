2d189450cac7092595ae47b2024885246f16e03d
dnlau96 <dnlau96@gmail.com>
2026-02-25 15:50:11 -0600

    EnRacha2 Second Commit

---
+++ app/src/main/java/com/example/enracha2/FinanzasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.enracha2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FinanzasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set the content view to your new layout
+        // setContentView(R.layout.activity_finanzas)
+    }
+}
--- app/src/main/java/com/example/enracha2/MainActivity.kt
+++ app/src/main/java/com/example/enracha2/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.enracha2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val finanzasButton: Button = findViewById(R.id.button2)
+        finanzasButton.setOnClickListener {
+            val intent = Intent(this, FinanzasActivity::class.java)
+            startActivity(intent)
+        }
+
+        val habitosButton: Button = findViewById(R.id.button3)
+        habitosButton.setOnClickListener {
+            val intent = Intent(this, HabitosActivity::class.java)
+            startActivity(intent)
         }
     }
 }
